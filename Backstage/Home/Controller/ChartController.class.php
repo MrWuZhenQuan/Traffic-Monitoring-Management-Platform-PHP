c89c7fa97cdae24c0e3f542b58306ecf390336b7
qax <jackic15@126.com>
2015-03-11 17:58:14 +0800

    add:人流量监控表前后台完成

--- Backstage/Home/Controller/ChartController.class.php
+++ Backstage/Home/Controller/ChartController.class.php
@@ -7,28 +7,59 @@ class ChartController extends CommonController {
     const DATABASE = 'traffic_data';
 
     public function index(){
-
+        $data = $this->getDiffentPeriodData($device_id);
+        p($data);die;
         $data = M(self::DATABASE)->select();
-        p($d = date('Y-m-d H:i:s',$data[0]['created_at']));
+        //p($d = date('Y-m-d H:i:s',$data[0]['created_at']));
         $array = array();
         $currentHour = date('H');
-        for ($i=0; $i < $currentHour ; $i++) { 
-            $chartData = array('period' => $i.'', 'visits' => $data['visiters'],'device' => '');
+        for ($i= 1; $i <= $currentHour ; $i++) {
+            $visiters[$i] = 0;
+            for ($j= 0; $j < count($data) ; $j++) {
+                $hour = date('H',$data[$j]['created_at']);
+                if($i == $hour)
+                    $visiters[$i] += $data[$j]['visiters'];
+            }
+            $chartData = array('period' => $i.'点', 'visits' => $visiters[$i],'device' => '');
             array_push($array,$chartData);
         }
         p($array);
-        p($data);die;
-        echo time();die;
-        $array = array();
-        for($i=1;$i<=24;$i++){
-            
-            $data = array('period' => $i."", 'visits' => $data['visiters'],'device' => '');
-            array_push($array,$data);
+        for ($i=0; $i <6 ; $i++) { 
+           p($d = date('H',$data[$i]['created_at'])); 
         }
-        
-        $time = time();
-        echo date('H:i:s');die;
+        die;
         $this->ajaxReturn($data);
     	$this->display();
+    }
+
+    private function getDiffentPeriodData($device_id){
+        $data = M(self::DATABASE)->select();
+        //p($d = date('Y-m-d H:i:s',$data[0]['created_at']));
+        $array = array();
+        $array = $this->createArrayModle();
+        $currentHour = date('H');
+        for ($i= 1; $i <= $currentHour ; $i++) {
+            $visiters[$i] = 0;
+            for ($j= 0; $j < count($data) ; $j++) {
+                $hour = date('H',$data[$j]['created_at']);
+                if($i == $hour)
+                    $visiters[$i] += $data[$j]['visiters'];
+            }
+            $array[$i-1] = array('period' => $i.'点', 'visits' => $visiters[$i],'device' => '');
+            // $chartData = array('period' => $i.'点', 'visits' => $visiters[$i],'device' => '');
+            // array_push($array,$chartData);
+        }
+
+        return $array;
+    }
+
+    private function createArrayModle(){
+        $arr = array();
+        for ($i=1; $i <= 24; $i++) { 
+            $arrModel = array('period' => $i.'点', 'visits' => '','device' => '');
+            array_push($arr,$arrModel);
+        }
+
+        return $arr;
     }
 }
