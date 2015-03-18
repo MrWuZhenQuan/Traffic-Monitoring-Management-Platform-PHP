c67d92d840aa86e11b6c1b9bf56605960504fa56
qax <qax@qax>
2015-03-18 16:33:50 +0800

    add: goods and goods_shelves

---
+++ mysql/tmp_goods.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- 主机: localhost
+-- 生成日期: 2015-03-18 16:19:16
+-- 服务器版本: 5.5.41-0ubuntu0.14.04.1
+-- PHP 版本: 5.5.9-1ubuntu4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- 数据库: `bs-tmp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `tmp_goods`
+--
+
+CREATE TABLE IF NOT EXISTS `tmp_goods` (
+  `id` int(11) NOT NULL,
+  `goods_type` varchar(128) CHARACTER SET utf8 NOT NULL,
+  `count` int(11) NOT NULL COMMENT '货物数量',
+  `shelves_id` int(11) NOT NULL,
+  `created_at` int(11) NOT NULL,
+  `updated_at` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='商品表';
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
